76f58533b23152b5103477f6d0a16ef848a90bd0
Incremnt <stevster27@gmail.com>
2025-09-10 16:22:56 +0400

    Update source.asm

--- source.asm
+++ source.asm
@@ -1,4 +1,19 @@
-;--- bf interpreter for Linux x86_64 by Denis Bazhenov (fasm btw) ---;
+; Copyright (C) 2025 Denis Bazhenov
+;
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;--- bf interpreter for Linux x86_64 by Incremnt (fasm btw) ---;
 format ELF64 executable
 entry _start
                                                                
